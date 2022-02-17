14de0db1fb507adf85cbb2a774fdae07ef94b4ec
Jason Benn <jasoncbenn@gmail.com>
2022-02-16 17:07:13 -0800

    main skeleton

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+# LOAD FROM NOTION
+# put into sqlite
+
+# FACEBOOK MESSENGER & RECENT FRIENDS
+# open FB and download relevant data to ~/Downloads (use selenium)
+# parse messenger events
+# parse recent friends
+
+# TWITTER
+# recent twitter DMs / interactions
+# include people on various lists
+# https://developer.twitter.com/en/docs/twitter-api/v1/direct-messages/sending-and-receiving/api-reference/get-event
+# CRM should have column for handles
+
+# CONTACT ENRICHMENT
+# anyone that hasn't had this done, do it
+
+# DUMP TO NOTION
+# i had something along these lines already
+
+# DUMP TO OMNI
